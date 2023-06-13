e5e4ff1df18f799887637f8b5ada36729cd2832e
Joseph Birr-Pixton <jpixton@gmail.com>
2023-06-13 15:43:43 +0100

    Move cipher.rs to new cipher module
    The intention is to delineate interfaces to be implemented by
    pluggable crypto providers.

--- rustls/src/lib.rs
+++ rustls/src/lib.rs
@@ -315,7 +315,6 @@ mod log {
 #[macro_use]
 mod msgs;
 mod anchors;
-mod cipher;
 mod common_state;
 mod conn;
 /// Crypto provider interface.
@@ -356,7 +355,7 @@ pub mod internal {
     }
     /// Low-level TLS message decryption functions.
     pub mod cipher {
-        pub use crate::cipher::MessageDecrypter;
+        pub use crate::crypto::cipher::MessageDecrypter;
     }
     /// Low-level TLS record layer functions.
     pub mod record_layer {
